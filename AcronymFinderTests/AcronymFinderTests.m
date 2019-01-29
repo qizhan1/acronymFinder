81ebcf243f23e63b1250a0eb77c7564368384806
Qi Zhan <qiz@zoosk.com>
2019-01-28 17:16:01 -0800

    revise unit test

--- AcronymFinderTests/AcronymFinderTests.m
+++ AcronymFinderTests/AcronymFinderTests.m
@@ -16,15 +16,22 @@
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    NSLog(@"Unit test ---- setUp");
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    NSLog(@"Unit test ----- tearDown");
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    NSLog(@"Unit test ----- testExample");
+    assert(true);
 }
 
 - (void)testPerformanceExample {
